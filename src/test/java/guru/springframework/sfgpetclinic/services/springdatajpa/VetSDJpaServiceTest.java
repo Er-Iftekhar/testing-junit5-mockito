ff5c66e9b8e50c4995707a5e93a977ae95fdc2b0
Iftekharuddin Syed <syedy636@gmail.com>
2024-09-05 13:49:33 +0200

    Add tests with mockito for the VetService implementation

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService service;
+
+    @Test
+    void deleteByIdTimes() {
+        service.deleteById(1L);
+        verify(vetRepository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtLeastOnce() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+        verify(vetRepository, atLeastOnce()).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtMost() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+        verify(vetRepository, atMost(5)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdNever() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+        verify(vetRepository, times(2)).deleteById(1L);
+        verify(vetRepository, never()).deleteById(5L);
+    }
+}
